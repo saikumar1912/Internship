e51f93b723ba3bbefb514ba05c04be8b03adc289
saikumar1912 <63963485+saikumar1912@users.noreply.github.com>
2020-04-22 21:56:12 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,3 +1,20 @@
+<html>
+    <head>
+        <title>Index</title>
+        <link rel="stylesheet" href="lstyle.css">
+    </head>
+    <body>
+        <div class="login" id="home">
+            <h1>Currency-Converter</h1>
+            <form name="f" method="POST" action="homepage.php">
+            <label for="Amount">AMOUNT</label><br>
+            <input type="number" placeholder="Enter value.." name="amt"/><br><br>
+            <label for="From">FROM:</label>
+            <input type="text" placeholder="ex:USD" name="frm"/>
+            <label for="to">To:</label>
+            <input type="text" name="to" placeholder="ex:INR"/><br><br>
+            <div class="submit"><input type="submit" name="convert" value="Convert" /></div><br><br>
+            </form>    
 <?php
 $curl=curl_init();
 $frm=$_POST['frm'];
@@ -12,3 +29,6 @@ $ans=($objects->rates->$to)*$_POST['amt'];
 echo "<p style='color:black;text-align:center;'>Your changed ".$frm." is equal to 
 <p style='color:RED;text-align:center; font-weight:bold;text-decoration: underline;'>".$ans." ".$to."</p></p>";
 ?>
+</div>
+    </body>
+</html>
